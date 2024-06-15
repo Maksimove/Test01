cc2ff10dd678db9e2edce866968004fc23593fa1
Maksimove <spamw7@icloud.com>
2024-06-15 14:14:55 +0300

    Initial Commit

---
+++ Test01/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Test01
+//
+//  Created by Evgeniy Maksimov on 15.06.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
