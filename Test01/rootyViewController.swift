43f7ded23c15925823ad57cb141cdc847a99fa7d
Maksimove <spamw7@icloud.com>
2024-06-15 14:44:20 +0300

    g

---
+++ Test01/rootyViewController.swift
@@ -0,0 +1,29 @@
+//
+//  rootyViewController.swift
+//  Test01
+//
+//  Created by Evgeniy Maksimov on 15.06.2024.
+//
+
+import UIKit
+
+class rootyViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
